5c5df7abf109205b62b52a42a510d7b7ce26d283
DESKTOP-ST0GILC\w2517 <25175731@qq.com>
2023-05-15 17:51:19 +0800

    vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,11 @@
-const rollupOptions = {
+/// <reference types="vitest" />
 
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import Unocss from "./config/unocss";
+
+const rollupOptions = {
   external: ["vue", "vue-router"],
   output: {
     globals: {
@@ -7,12 +13,6 @@ const rollupOptions = {
     },
   },
 };
-
-
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import Unocss from "./config/unocss";
 
 // https://vitejs.dev/config/
 
@@ -23,8 +23,19 @@ export default defineConfig({
     // options are passed on to @vue/babel-plugin-jsx
     vueJsx(),
     // 添加UnoCSS插件
-    Unocss()],
-
+    Unocss()
+  ],
+  test: {
+    // enable jest-like global test APIs
+    globals: true,
+    // simulate DOM with happy-dom
+    // (requires installing happy-dom as a peer dependency)
+    environment: 'happy-dom',
+    // 支持tsx组件，很关键
+    transformMode: {
+      web: [/.[tj]sx$/]
+    }
+  },
   // 添加库模式配置
   build: {
     rollupOptions,
